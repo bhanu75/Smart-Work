698219477869e92bc63d7a981e1a1668fa787968
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-12-24 22:44:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser'
-  },
   server: {
     port: 3000,
-    host: true
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 })
