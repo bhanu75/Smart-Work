3e4b21769c2dac4a0e0442d0c1e4fea65705dcad
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-12-25 20:03:31 +0530

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App1 from "./apps/app1/App"
-import App2 from "./apps/app2/App"
-import App3 from "./apps/app3/App"
+import App1 from "./apps/app1/App.jsx";
+import App2 from "./apps/app2/App.jsx";
+import App3 from "./apps/app3/App.jsx";
 
 export default function Router() {
   return (
@@ -13,5 +13,5 @@ export default function Router() {
         <Route path="/app3/*" element={<App3 />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
