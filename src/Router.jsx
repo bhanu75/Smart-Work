74fdf665b78d7ee0df246ae71f7601ac11beb9c3
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-12-25 20:41:12 +0530

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import App1 from "./apps/app1/App.jsx";
 import App2 from "./apps/app2/App.jsx";
@@ -8,6 +8,9 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* DEFAULT ROUTE */}
+        <Route path="/" element={<Navigate to="/app1" replace />} />
+
         <Route path="/app1/*" element={<App1 />} />
         <Route path="/app2/*" element={<App2 />} />
         <Route path="/app3/*" element={<App3 />} />
